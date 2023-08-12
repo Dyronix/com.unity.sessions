92a9eb8107f8698fdce2d23e1f2e1b90507b79c4
Dave De Breuck <dave.debreuck@twikit.com>
2023-08-12 19:46:26 +0200

    Upload com.unity.sessions

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# com.unity.sessions
-Binding a unique player ID to a session
+# Unity Sessions
+
+## summary
+This class uses a unique player ID to bind a player to a session. Once that player connects to a host, the host
+associates the current ClientID to the player's unique ID. If the player disconnects and reconnects to the same
+host, the session is preserved.
+
+## remarks
+Using a client-generated player ID and sending it directly could be problematic, as a malicious user could
+intercept it and reuse it to impersonate the original user. We are currently investigating this to offer a
+solution that handles security better.
+
+## Reference
+https://github.com/Unity-Technologies/com.unity.multiplayer.samples.coop
---
+++ Source/SessionPlayerData.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Unity.Sessions
+{
+    public struct SessionPlayerData : ISessionPlayerData
+    {
+        public string PlayerName;
+        public int PlayerNumber;
+        public Vector3 PlayerPosition;
+        public Quaternion PlayerRotation;
+        public bool HasCharacterSpawned;
+
+        public SessionPlayerData(ulong clientID, string name, bool isConnected = false, bool hasCharacterSpawned = false)
+        {
+            ClientID = clientID;
+            PlayerName = name;
+            PlayerNumber = -1;
+            PlayerPosition = Vector3.zero;
+            PlayerRotation = Quaternion.identity;
+            IsConnected = isConnected;
+            HasCharacterSpawned = hasCharacterSpawned;
+        }
+
+        public bool IsConnected { get; set; }
+        public ulong ClientID { get; set; }
+
+        public void Reinitialize()
+        {
+            HasCharacterSpawned = false;
+        }
+    }
+}
